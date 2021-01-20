5219c2f0c4c141300afaddb5f4006aa248644001
John Ottenlips <jottenlips@gmail.com>
2021-01-20 09:38:45 -0600

    more linking trials

--- packages/bones_react_native_app/babel.config.js
+++ packages/bones_react_native_app/babel.config.js
@@ -1,23 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  alias: {
-    react: require.resolve('react', {
-      paths: [path.join(__dirname, './')],
-    }),
-    '^react-native$': require.resolve('react-native', {
-      paths: [path.join(__dirname, './')],
-    }),
-    '^react-native/(.+)': ([, name]) =>
-      require.resolve(`react-native/${name}`, {
-        paths: [path.join(__dirname, './')],
-      }),
-    // '^@react-navigation/(.+)': ([, name]) =>
-    //   require.resolve(`@react-navigation/${name}`, {
-    //     paths: [path.join(__dirname, './')],
-    //   }),
-    '^react-spring/(.+)': ([, name]) =>
-      require.resolve(`react-spring/${name}`, {
-        paths: [path.join(__dirname, './')],
-      }),
-  },
 };
--- packages/bones_react_native_app/metro.config.js
+++ packages/bones_react_native_app/metro.config.js
@@ -15,36 +15,32 @@
 //     }),
 //   },
 // };
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-// const path = require('path');
-
-// const watchFolders = [path.resolve(__dirname + '/..')];
-
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
 const path = require('path');
-const watchFolders = [
-  path.resolve(__dirname + '/..'), //Relative path to packages directory
-  path.resolve(__dirname + '/../../node_modules'), //Relative path to packages directory
-];
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+const currentDir = __dirname;
+
+const config = {
+  watchFolders: [
+    // To allow finding files outside mobile
+    path.resolve(currentDir, '..'),
+  ],
+
+  resolver: {
+    extraNodeModules: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(currentDir, 'node_modules/react-native'),
+      'react-spring': path.resolve(currentDir, 'node_modules/react-spring'),
+      // 'react-navigation': path.resolve(
+      //   currentDir,
+      //   'node_modules/react-navigation',
+      // ),
+      // 'react-native-vector-icons': path.resolve(
+      //   currentDir,
+      //   'node_modules/react-native-vector-icons',
+      // ),
+      // '@babel/runtime': path.resolve(currentDir, 'node_modules/@babel/runtime'),
+    },
   },
-  watchFolders,
 };
+
+module.exports = config;
